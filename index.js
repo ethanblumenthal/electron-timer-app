878eca0992899a34220ee3d64d1e63d68b202913
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-21 14:14:10 -0500

    refactored to create main window sub class

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const path = require('path')
 const electron = require('electron')
-const { app, BrowserWindow, Tray } = electron
+const { app } = electron
 const TimerTray = require('./app/timer_tray')
+const MainWindow = require('./app/main_window')
 
 let mainWindow, tray
 
 app.on('ready', () => {
   app.dock.hide()
-  mainWindow = new BrowserWindow({
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false
-  })
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
-  mainWindow.on('blur', () => {
-    mainWindow.hide()
-  })
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`)
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
