d4b9d33fd3f958f79fc8369f37a7112dfdac9d07
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-21 13:26:48 -0500

    added tray to access app

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+const path = require('path')
 const electron = require('electron')
-const { app, BrowserWindow } = electron
-let mainWindow
+const { app, BrowserWindow, Tray } = electron
+
+let mainWindow, tray
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+  tray = new Tray(iconPath)
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds
+    const { height, width } = mainWindow.getBounds()
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width
+      })
+      mainWindow.show()
+    }
+  })
 })
