36b48003dc314568744c8a7edd9b746416b74620
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-21 14:22:10 -0500

    added timer to task bar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const electron = require('electron')
-const { app } = electron
+const { app, ipcMain } = electron
 const TimerTray = require('./app/timer_tray')
 const MainWindow = require('./app/main_window')
 
@@ -13,4 +13,8 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
   tray = new TimerTray(iconPath, mainWindow)
+})
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+  tray.setTitle(timeLeft)
 })
